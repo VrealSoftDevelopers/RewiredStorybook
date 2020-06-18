ff1f7b36e716a9ded6f0bd28d932ced640294eeb
unknown <sashakulinich97@gmail.com>
2020-06-18 16:39:32 +0300

    update

---
+++ precache-manifest.d137bc5813213a0afe5eb4fb256f0a98.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100c950ec59fe39891132dac7017d4ec",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.423a888385803c0364b1.bundle.js"
+  },
+  {
+    "url": "runtime~main.423a888385803c0364b1.bundle.js"
+  },
+  {
+    "revision": "09672320a8391eb9e765",
+    "url": "static/css/main.3172564e.chunk.css"
+  },
+  {
+    "revision": "002f6a81a685aee01ee3cbb66cb9a2c2",
+    "url": "static/media/icons.002f6a81.ttf"
+  },
+  {
+    "revision": "77d69e2abe70383944fe8102e7efa9f9",
+    "url": "static/media/icons.77d69e2a.woff2"
+  },
+  {
+    "revision": "79a50d540a8b9fdc6ae45f7b0966fcb4",
+    "url": "static/media/icons.79a50d54.woff"
+  },
+  {
+    "revision": "a3efd32b7bbc686b6c775e6d5f2d68ca",
+    "url": "static/media/icons.a3efd32b.eot"
+  },
+  {
+    "revision": "eaeb15f6c7897d11c8629260c15575a5",
+    "url": "static/media/icons.eaeb15f6.svg"
+  },
+  {
+    "url": "vendors~main.423a888385803c0364b1.bundle.js"
+  },
+  {
+    "url": "vendors~main.423a888385803c0364b1.bundle.js.LICENSE.txt"
+  }
+]);
